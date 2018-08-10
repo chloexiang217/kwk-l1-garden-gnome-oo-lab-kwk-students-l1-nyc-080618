116619fb9dccee175c6b135dae6da4605f08426e
chloexiang217 <chloe.xiang217@gmail.com>
2018-08-10 15:37:02 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -37,7 +37,7 @@ class GardenGnome
   def shout
     return "GNARLY!!!"
   end
-
+end
 def introduce_self
   return "Hello humans, my name is #{name}, I am #{age} years old, and you'll rue the day you crossed me!"
 end
